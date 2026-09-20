bf3c0f2c70480a0966f364f857f00445b7710da3
derickgabrielschumacherdealmei-stack <derickgabrielschumacherdealmei@gmail.com>
2026-09-20 14:42:54 -0300

    Update model name path in GenerativeModel initialization

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name='gemini-1.5-flash-',
+    model_name='models/gemini-1.5-flash',
     system_instruction=system_instruction,
-    generation_config=generation_config
+    generation_config=generation_config,
 )
 
 # Inicializa o histórico de mensagens do chat na sessão do Streamlit
