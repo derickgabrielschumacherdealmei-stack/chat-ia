b89736d5374a2aa2e256fb7bf3584d4c76983a49
derickgabrielschumacherdealmei-stack <derickgabrielschumacherdealmei@gmail.com>
2026-09-20 14:33:03 -0300

    Fix model name in GenerativeModel initialization

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name='gemini-1.5-flash-latest',
+    model_name='gemini-1.5-flash-',
     system_instruction=system_instruction,
     generation_config=generation_config
 )
