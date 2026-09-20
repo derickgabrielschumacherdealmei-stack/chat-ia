00c105eb0735949e8fb7c6864aec0c12da3474c3
derickgabrielschumacherdealmei-stack <derickgabrielschumacherdealmei@gmail.com>
2026-09-20 14:27:18 -0300

    Update model name to 'gemini-1.5-flash-latest'

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name='gemini-1.5-flash',
+    model_name='gemini-1.5-flash-latest'
     system_instruction=system_instruction,
     generation_config=generation_config
 )
