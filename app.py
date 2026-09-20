ecee6fb62766f56853bcfb7e188df35a89d9783e
derickgabrielschumacherdealmei-stack <derickgabrielschumacherdealmei@gmail.com>
2026-09-20 14:58:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,11 +45,10 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name='models/gemini-1.5-flash',
+    model_name='gemini-1.5-flash',
     system_instruction=system_instruction,
     generation_config=generation_config,
 )
-
 # Inicializa o histórico de mensagens do chat na sessão do Streamlit
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = model.start_chat(history=[])
